f5948f52a550bfa5dac97509fcff7c7810114119
Akahad1 <aksahad234567@gmail.com>
2024-06-27 21:56:12 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <button className=" m-4 text-xl p-5 bg-green-500 text-white rounded-md">
+        Incriment
+      </button>
+      <h1>0</h1>
+      <button className=" m-4 text-xl p-5 bg-red-500 text-white rounded-md">
+        Decriment
+      </button>
+    </div>
+  );
+}
+
+export default App;
